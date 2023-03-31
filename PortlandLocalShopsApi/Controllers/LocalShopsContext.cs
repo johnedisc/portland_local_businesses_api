bf5fb76a9354d677eae2bd0d5abce58e02a21713
johnedisc <christopher.johnedis@gmail.com>
2023-03-31 11:12:48 -0700

    seed database for convenience

--- PortlandLocalShopsApi/Controllers/LocalShopsContext.cs
+++ PortlandLocalShopsApi/Controllers/LocalShopsContext.cs
@@ -11,4 +11,14 @@ public class PortlandLocalShopsApiContext : DbContext
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Shop>()
+        .HasData(
+          new Shop { ShopId = 1, Name = "doug fir lounge", StoreType = "music venue", MusicType = "various, singer-songwriter, guitar-bass-drums" PartOfTown = "e, east" },
+          new Shop { ShopId = 2, Name = "mississippi pizza", StoreType = "pizza restaurant, music venue", MusicType = "various, salsa, timba, singer-songwriter, guitar-bass-drums", PartOfTown = "ne, northeast" },
+          new Shop { ShopId = 3, Name = "the goodfoot", StoreType = "bar, music venue", MusicType = "various, funk, salsa, guitar-bass-drums", PartOfTown = "ne, northeast" }
+          new Shop { ShopId = 4, Name = "the 1905", StoreType = "bar, pizza, music venue", MusicType = "jazz", PartOfTown = "ne, northeast" }
+        );
+    }
 }
