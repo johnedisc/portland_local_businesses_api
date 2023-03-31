6a7cec74ef85faabd16b8a3d3e78a5266c81c964
johnedisc <christopher.johnedis@gmail.com>
2023-03-31 10:42:16 -0700

    add migration

---
+++ PortlandLocalShopsApi/Migrations/20230331174102_addMusicType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortlandLocalShopsApi.Migrations
+{
+    public partial class addMusicType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MusicType",
+                table: "Shops",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MusicType",
+                table: "Shops");
+        }
+    }
+}
