16a92fe66c5ee789bbe78b18fee508420bda269f
johnedisc <christopher.johnedis@gmail.com>
2023-03-31 09:48:54 -0700

    create initial migrations and update. add get, get/{id}, and post to the controller

--- PortlandLocalShopsApi/Controllers/ShopsController.cs
+++ PortlandLocalShopsApi/Controllers/ShopsController.cs
@@ -1,3 +1,49 @@
-[ApiController]
-[Route("[controller]")]
-public class PortlandLocalShopsApi : ControllerBase
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortlandLocalShopsApi.Models;
+
+namespace CretaceousApi.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class PortlandLocalShopsApiController : ControllerBase
+  {
+    private readonly PortlandLocalShopsApiContext _db;
+
+    public PortlandLocalShopsApiController(PortlandLocalShopsApiContext db)
+    {
+      _db = db;
+    }
+
+    // get: shops
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Shop>>> Get()
+    {
+      return await _db.Shops.ToListAsync();
+    }
+
+    // get: shops/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Shop>> GetShop(int id)
+    {
+      Shop foundShopInstance = await _db.Shops.FindAsync(id);
+
+      if (foundShopInstance == null)
+      {
+        return NotFound();
+      }
+
+      return foundShopInstance;
+    }
+
+    [HttpPost]
+public async Task<ActionResult<Shop>> PostTodoItem(Shop postedShopInstance)
+{
+    _db.Shops.Add(postedShopInstance);
+    await _db.SaveChangesAsync();
+
+    //    nameof just means: make a string of the action method to avoid hard-coding?
+    return CreatedAtAction(nameof(GetShop), new { id = postedShopInstance.ShopId }, postedShopInstance);
+}
+  }
+}
---
+++ PortlandLocalShopsApi/Migrations/20230331163151_initialMigrationFromSimpleModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortlandLocalShopsApi.Migrations
+{
+    public partial class initialMigrationFromSimpleModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    ShopId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    StoreType = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PartOfTown = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.ShopId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shops");
+        }
+    }
+}
