eb45eea13cdbb8c24f4aeb84530d7689e1977be0
johnedisc <christopher.johnedis@gmail.com>
2023-03-31 14:49:06 -0700

    update ShopsController.cs
    object punctation incorrect

--- PortlandLocalShopsApi/Controllers/LocalShopsContext.cs
+++ PortlandLocalShopsApi/Controllers/LocalShopsContext.cs
@@ -15,9 +15,9 @@ public class PortlandLocalShopsApiContext : DbContext
     {
       builder.Entity<Shop>()
         .HasData(
-          new Shop { ShopId = 1, Name = "doug fir lounge", StoreType = "music venue", MusicType = "various, singer-songwriter, guitar-bass-drums" PartOfTown = "e, east" },
+          new Shop { ShopId = 1, Name = "doug fir lounge", StoreType = "music venue", MusicType = "various, singer-songwriter, guitar-bass-drums", PartOfTown = "e, east" },
           new Shop { ShopId = 2, Name = "mississippi pizza", StoreType = "pizza restaurant, music venue", MusicType = "various, salsa, timba, singer-songwriter, guitar-bass-drums", PartOfTown = "ne, northeast" },
-          new Shop { ShopId = 3, Name = "the goodfoot", StoreType = "bar, music venue", MusicType = "various, funk, salsa, guitar-bass-drums", PartOfTown = "ne, northeast" }
+          new Shop { ShopId = 3, Name = "the goodfoot", StoreType = "bar, music venue", MusicType = "various, funk, salsa, guitar-bass-drums", PartOfTown = "ne, northeast" },
           new Shop { ShopId = 4, Name = "the 1905", StoreType = "bar, pizza, music venue", MusicType = "jazz", PartOfTown = "ne, northeast" }
         );
     }
