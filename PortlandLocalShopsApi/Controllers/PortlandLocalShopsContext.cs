5c2974704aaf8fb07338d4f5439a526999f12a0c
johnedisc <christopher.johnedis@gmail.com>
2023-03-31 17:07:41 -0700

    update context.cs
    alter dataseeding to populate db

--- PortlandLocalShopsApi/Controllers/PortlandLocalShopsContext.cs
+++ PortlandLocalShopsApi/Controllers/PortlandLocalShopsContext.cs
@@ -12,20 +12,13 @@ public class PortlandLocalShopsApiContext : DbContext
 
   protected override void OnModelCreating(ModelBuilder builder)
   {
-    builder.Entity<Shop>()
-      .HasData(
-          new Shop { ShopId = 1, Name = "doug fir lounge", StoreType = "music venue", MusicType = "various, singer-songwriter, guitar-bass-drums", PartOfTown = "e, east" },
-          new Shop { ShopId = 2, Name = "mississippi pizza", StoreType = "pizza restaurant, music venue", MusicType = "various, salsa, timba, singer-songwriter, guitar-bass-drums", PartOfTown = "ne, northeast" },
-          new Shop { ShopId = 3, Name = "the goodfoot", StoreType = "bar, music venue", MusicType = "various, funk, salsa, guitar-bass-drums", PartOfTown = "ne, northeast" },
-          new Shop { ShopId = 4, Name = "the 1905", StoreType = "bar, pizza, music venue", MusicType = "jazz", PartOfTown = "ne, northeast" },
-          for (int i = 5; i < 100; i++;)
-          {
-            new Shop { ShopId=Faker.Number}
-
-user.Name = Faker.Name.FullName(NameFormats.WithPrefix);
-          }
-
+    for (int i = 5; i < 100; i++)
+    {
+      builder.Entity<Shop>().HasData(
+          new Shop { ShopId = i, Name = Faker.Company.Name(),  StoreType = $"Faker.Lorem.Words(4)", MusicType = $"Faker.Lorem.Words(4)", PartOfTown = $"Faker.Lorem.Words(4)" }
           );
+    }
+
   }
 }
   
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://localhost:7263;http://localhost:5250
+
+
+new Shop { ShopId = 1, Name = "doug fir lounge", StoreType = "music venue", MusicType = "various, singer-songwriter, guitar-bass-drums", PartOfTown = "e, east" }, new Shop { ShopId = 2, Name = "mississippi pizza", StoreType = "pizza restaurant, music venue", MusicType = "various, salsa, timba, singer-songwriter, guitar-bass-drums", PartOfTown = "ne, northeast" }, new Shop { ShopId = 3, Name = "the goodfoot", StoreType = "bar, music venue", MusicType = "various, funk, salsa, guitar-bass-drums", PartOfTown = "ne, northeast" }, new Shop { ShopId = 4, Name = "the 1905", StoreType = "bar, pizza, music venue", MusicType = "jazz", PartOfTown = "ne, northeast" }
